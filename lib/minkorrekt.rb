f9f7733520a5baa339c9f5723b068d0bfe4fd487
texhnolyze <joegriepen@gmail.com>
2018-06-17 16:14:23 +1000

    refac(minkorrekt): cleanup dir structure and setup

--- lib/minkorrekt.rb
+++ lib/minkorrekt.rb
@@ -4,73 +4,61 @@ module Minkorrekt
   require 'nokogiri'
   require 'open-uri'
 
-  require_relative 'minkorrekt/feed_parser'
-  require_relative 'minkorrekt/episode_extractor'
-  require_relative 'minkorrekt/episode'
-  require_relative 'minkorrekt/experiment'
-  require_relative 'minkorrekt/china_gadget'
+  require_relative 'minkorrekt/importer/feed_parser'
+  require_relative 'minkorrekt/importer/episode_extractor'
+
+  require_relative 'minkorrekt/model/episode'
+  require_relative 'minkorrekt/model/experiment'
+  require_relative 'minkorrekt/model/china_gadget'
+  require_relative 'minkorrekt/model/description_strategy'
+  require_relative 'minkorrekt/model/title_strategy'
+
   require_relative 'minkorrekt/github_client'
-  require_relative 'minkorrekt/title_strategy'
-  require_relative 'minkorrekt/description_strategy'
+
+  module_function
 
   def setup(feed_uri = 'http://minkorrekt.de/feed/mp3/')
+    episodes = episode_importer(feed_uri).generate_episode_models
+  end
+
+  def github_client
     stack = Faraday::RackBuilder.new do |builder|
       builder.use Faraday::HttpCache, serializer: Marshal, shared_cache: false
       builder.use Octokit::Response::RaiseError
       builder.adapter Faraday.default_adapter
     end
     Octokit.middleware = stack
+
     github_backend = Octokit::Client
     github_oauth_token = ENV['GITHUB_OAUTH_TOKEN']
-    github_client = Minkorrekt::GithubClient.setup(
+
+    Minkorrekt::GithubClient.setup(
       github_backend,
       github_oauth_token
     )
+  end
 
-    parser = Minkorrekt::FeedParser.new(feed_uri)
+  def episode_importer(uri)
+    parser = Minkorrekt::FeedParser.new(uri)
+    Minkorrekt::EpisodeExtractor.new(parser, episode)
+  end
 
-    experiment = Minkorrekt::Experiment.setup(
+  def episode
+    Minkorrekt::Episode.setup(experiment, china_gadget)
+  end
+
+  def experiment
+    Minkorrekt::Experiment.setup(
       Minkorrekt::ExperimentTitleStrategy,
       Minkorrekt::ExperimentDescriptionStrategy,
       github_client
     )
-    china_gadget = Minkorrekt::ChinaGadget.setup(
+  end
+
+  def china_gadget
+    Minkorrekt::ChinaGadget.setup(
       Minkorrekt::ChinaGadgetTitleStrategy,
       Minkorrekt::ChinaGadgetDescriptionStrategy
     )
-
-    episode = Minkorrekt::Episode.setup(experiment, china_gadget)
-
-    extractor = Minkorrekt::EpisodeExtractor.new(parser, episode)
-    episodes = extractor.generate_episode_models
-
-    episode = episodes[24]
-    puts episode.title
-    puts "\nExperiment:"
-    puts episode.experiment.title
-    puts episode.experiment.description
-    puts "\nChinaGadget:"
-    puts episode.china_gadget.title
-    puts episode.china_gadget.description
-
-    episode2 = episodes[63]
-    puts episode2
-    puts "\nExperiment:"
-    puts episode2.experiment.title
-    puts episode2.experiment.description
-    puts "\nChinaGadget:"
-    puts episode2.china_gadget.title
-    puts episode2.china_gadget.description
-
-    episode3 = episodes[87]
-    puts episode3
-    puts "\nExperiment:"
-    puts episode3.experiment.title
-    puts episode3.experiment.description
-    puts "\nChinaGadget:"
-    puts episode3.china_gadget.title
-    puts episode3.china_gadget.description
   end
-
-  module_function :setup
 end
